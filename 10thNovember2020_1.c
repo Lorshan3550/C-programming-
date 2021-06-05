0bff789058bbabc608101bdfc351900b4b91cf41
Lorshan3550 <73054467+Lorshan3550@users.noreply.github.com>
2021-06-05 18:25:33 +0530

    Add files via upload

---
+++ 10thNovember2020_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int marks;
+	printf("Enter the mark:");
+	scanf("%d",&marks);
+	if(marks>=75 && marks<=100)
+	{
+		printf("Your grade is A\n");	
+	}
+	else if(marks>=60 && marks<75)
+	{
+		printf("Your grade is B\n");	
+	}
+	else if(marks>)
+}
